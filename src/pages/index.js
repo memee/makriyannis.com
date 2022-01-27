671e9c7a84b76762dd01806099bee8320b3407e6
Maciej Maciaszek <maciej.maciaszek@gmail.com>
2022-01-27 14:48:13 +0100

    Change casing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,38 @@
-import React from 'react'
-import { graphql } from "gatsby";
+import React from "react";
 
-import Layout from '../components/layout'
-import Header from '../components/Header'
-import Main from '../components/Main'
-import Footer from '../components/Footer'
+import Layout from "../components/layout";
+import Header from "../components/header";
+import Main from "../components/main";
+import Footer from "../components/footer";
 
 class IndexPage extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       isArticleVisible: false,
       timeout: false,
       articleTimeout: false,
-      article: '',
-      loading: 'is-loading'
-    }
-    this.handleOpenArticle = this.handleOpenArticle.bind(this)
-    this.handleCloseArticle = this.handleCloseArticle.bind(this)
+      article: "",
+      loading: "is-loading",
+    };
+    this.handleOpenArticle = this.handleOpenArticle.bind(this);
+    this.handleCloseArticle = this.handleCloseArticle.bind(this);
     this.setWrapperRef = this.setWrapperRef.bind(this);
     this.handleClickOutside = this.handleClickOutside.bind(this);
   }
 
-  componentDidMount () {
+  componentDidMount() {
     this.timeoutId = setTimeout(() => {
-      this.setState({loading: ''});
+      this.setState({ loading: "" });
     }, 100);
-    document.addEventListener('mousedown', this.handleClickOutside);
+    document.addEventListener("mousedown", this.handleClickOutside);
   }
 
-  componentWillUnmount () {
+  componentWillUnmount() {
     if (this.timeoutId) {
       clearTimeout(this.timeoutId);
     }
-    document.removeEventListener('mousedown', this.handleClickOutside);
+    document.removeEventListener("mousedown", this.handleClickOutside);
   }
 
   setWrapperRef(node) {
@@ -41,45 +40,41 @@ class IndexPage extends React.Component {
   }
 
   handleOpenArticle(article) {
-
     this.setState({
       isArticleVisible: !this.state.isArticleVisible,
-      article
-    })
+      article,
+    });
 
     setTimeout(() => {
       this.setState({
-        timeout: !this.state.timeout
-      })
-    }, 325)
+        timeout: !this.state.timeout,
+      });
+    }, 325);
 
     setTimeout(() => {
       this.setState({
-        articleTimeout: !this.state.articleTimeout
-      })
-    }, 350)
-
+        articleTimeout: !this.state.articleTimeout,
+      });
+    }, 350);
   }
 
   handleCloseArticle() {
-
     this.setState({
-      articleTimeout: !this.state.articleTimeout
-    })
+      articleTimeout: !this.state.articleTimeout,
+    });
 
     setTimeout(() => {
       this.setState({
-        timeout: !this.state.timeout
-      })
-    }, 325)
+        timeout: !this.state.timeout,
+      });
+    }, 325);
 
     setTimeout(() => {
       this.setState({
         isArticleVisible: !this.state.isArticleVisible,
-        article: ''
-      })
-    }, 350)
-
+        article: "",
+      });
+    }, 350);
   }
 
   handleClickOutside(event) {
@@ -93,9 +88,16 @@ class IndexPage extends React.Component {
   render() {
     return (
       <Layout location={this.props.location} data={this.props.data}>
-        <div className={`body ${this.state.loading} ${this.state.isArticleVisible ? 'is-article-visible' : ''}`}>
+        <div
+          className={`body ${this.state.loading} ${
+            this.state.isArticleVisible ? "is-article-visible" : ""
+          }`}
+        >
           <div id="wrapper">
-            <Header onOpenArticle={this.handleOpenArticle} timeout={this.state.timeout} />
+            <Header
+              onOpenArticle={this.handleOpenArticle}
+              timeout={this.state.timeout}
+            />
             <Main
               isArticleVisible={this.state.isArticleVisible}
               timeout={this.state.timeout}
@@ -109,9 +111,8 @@ class IndexPage extends React.Component {
           <div id="bg"></div>
         </div>
       </Layout>
-    )
+    );
   }
 }
 
-export default IndexPage
-
+export default IndexPage;
